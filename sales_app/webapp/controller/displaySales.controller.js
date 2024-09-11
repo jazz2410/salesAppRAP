65e02500ea98d52fd2c8fd148ff4b1a4f21f32a1
Dhaliwal <tajinder.dhaliwal@accenture.com>
2024-09-11 13:08:00 +0200

    Added JSON display functionality

--- sales_app/webapp/controller/displaySales.controller.js
+++ sales_app/webapp/controller/displaySales.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
     'sap/ui/model/Filter',
     'sap/ui/model/FilterOperator',
 ],
-    function (Controller, JSONModel, MessageToast,Filter,FilterOperator) {
+    function (Controller, JSONModel, MessageToast, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("salesapp.controller.displaySales", {
@@ -25,12 +25,12 @@ sap.ui.define([
                 this.getView().setModel(changeHeaderValuesModel, 'changeHeaderValuesModel');
 
                 var createDeliveryValues = {
-                    ShippingPoint : undefined,
-                    Quantity : undefined,
+                    ShippingPoint: undefined,
+                    Quantity: undefined,
                 }
 
                 var createDeliveryValuesModel = new JSONModel(createDeliveryValues);
-                this.getView().setModel(createDeliveryValuesModel,'createDeliveryValuesModel');
+                this.getView().setModel(createDeliveryValuesModel, 'createDeliveryValuesModel');
 
             },
             onSalesHeaderClick: function (oEvent) {
@@ -53,12 +53,12 @@ sap.ui.define([
                 changeHeaderValuesModel.setProperty('/PurchaseOrderByCustomer', newPurchaseOrderByCustomer);
 
             },
-            onChangeShippingPoint: function(event){
+            onChangeShippingPoint: function (event) {
                 var createDeliveryValuesModel = this.getView().getModel('createDeliveryValuesModel');
                 var newShippingPoint = event.getParameter('newValue');
                 createDeliveryValuesModel.setProperty('/ShippingPoint', newShippingPoint);
             },
-            onChangeQuantity: function(event){
+            onChangeQuantity: function (event) {
                 var createDeliveryValuesModel = this.getView().getModel('createDeliveryValuesModel');
                 var newQuantity = event.getParameter('newValue');
                 createDeliveryValuesModel.setProperty('/Quantity', newQuantity);
@@ -86,16 +86,16 @@ sap.ui.define([
 
                         var message = oResponse.headers['sap-message'];
                         var message_obj = JSON.parse(message);
-                        if(message_obj.details.length != 0){
-                            var content = message_obj.details.map(function(element){
+                        if (message_obj.details.length != 0) {
+                            var content = message_obj.details.map(function (element) {
                                 return new sap.m.StandardListItem({
-                                    title : element.message,
+                                    title: element.message,
                                     //description : element.message,
                                 })
                             });
                         }
-      
-                        this._showMessageDialog(message_obj.message,message_obj.severity,content)
+
+                        this._showMessageDialog(message_obj.message, message_obj.severity, content)
                         this.getView().getModel().refresh(true);
                         this._resetModel();
 
@@ -106,10 +106,10 @@ sap.ui.define([
                     }
                 });
             },
-            _showMessageDialog(sTitle,sState,sContent) {
+            _showMessageDialog(sTitle, sState, sContent) {
 
                 var status;
-                switch(sState){
+                switch (sState) {
                     case 'success':
                         status = sap.ui.core.ValueState.Success;
                         break;
@@ -136,127 +136,171 @@ sap.ui.define([
                                 oMessageDialog.close()
                             }
                         }),
-                    afterClose: function(){
+                    afterClose: function () {
                         oMessageDialog.destroy();
-                    }    
+                    }
                 });
                 oMessageDialog.open();
             },
-            _resetModel(){
+            _resetModel() {
                 var changeHeaderStateModel = this.getView().getModel('changeHeaderStateModel');
                 changeHeaderStateModel.setProperty('/editable', false);
                 var changeHeaderValuesModel = this.getView().getModel('changeHeaderValuesModel');
                 changeHeaderValuesModel.setProperty('/PurchaseOrderByCustomer', undefined);
             },
-            onDeliveryShow: function(){
-                   var item = this.byId('table').getSelectedItems()[0];
-
-                    if(!item){
-                        MessageToast.show("Select a sales order item");
-                        return;
-                    }
-                    var sPath = item.getBindingContext().getPath();
-                    var itemsPath = sPath + '/to_Deliveries';
-                    if(!this._oDialog){
-                        this._oDialog = sap.ui.xmlfragment(this.getView().getId(),"salesapp.view.deliveries", this);
-                        this.getView().addDependent(this._oDialog);
-                    }
-                    this.getView().byId('deliverySmartTable').setTableBindingPath(itemsPath);
-                    this.getView().byId("deliverySmartTable").rebindTable(true);
-                    this._oDialog.open();
-            },
-            closeDelivery: function(){
-                this._closeDialog();
-            },
-            closeDeliveryCreate: function(){
-                this._closeDialog();
-            },
-            onDeliveryCreate: function(){
+            onDeliveryShow: function () {
                 var item = this.byId('table').getSelectedItems()[0];
 
-                if(!item){
+                if (!item) {
+                    MessageToast.show("Select a sales order item");
+                    return;
+                }
+                var sPath = item.getBindingContext().getPath();
+                var itemsPath = sPath + '/to_Deliveries';
+                if (!this._oDialog) {
+                    this._oDialog = sap.ui.xmlfragment(this.getView().getId(), "salesapp.view.deliveries", this);
+                    this.getView().addDependent(this._oDialog);
+                }
+                this.getView().byId('deliverySmartTable').setTableBindingPath(itemsPath);
+                this.getView().byId("deliverySmartTable").rebindTable(true);
+                this._oDialog.open();
+            },
+            closeDelivery: function () {
+                this._closeDialog();
+            },
+            closeDeliveryCreate: function () {
+                this._closeDialog();
+            },
+            onDeliveryCreate: function () {
+                var item = this.byId('table').getSelectedItems()[0];
+
+                if (!item) {
                     MessageToast.show("Select a sales order item");
                     return;
                 }
                 var sPath = item.getBindingContext().getPath(); //Get path order sales order items
 
-                if(!this._oDialog){
-                    this._oDialog = sap.ui.xmlfragment(this.getView().getId(),"salesapp.view.deliveryCreate", this);
+                if (!this._oDialog) {
+                    this._oDialog = sap.ui.xmlfragment(this.getView().getId(), "salesapp.view.deliveryCreate", this);
                     this.getView().addDependent(this._oDialog);
                 }
                 this.getView().byId('deliveryCreateForm').bindElement(sPath);
                 this._oDialog.open();
 
             },
-            onPressDeliveryCreate: function(){
+            onPressDeliveryCreate: function () {
                 var createDeliveryValuesModel = this.getView().getModel('createDeliveryValuesModel');
                 var newShippingPoint = createDeliveryValuesModel.getProperty('/ShippingPoint');
                 var newQuantity = createDeliveryValuesModel.getProperty('/Quantity');
 
                 var sPath = '/ZTD_DELIVERIES';
                 var data = {
-                    ShippingPoint : newShippingPoint,
-                    ActualDeliveryQuantity : newQuantity,
+                    ShippingPoint: newShippingPoint,
+                    ActualDeliveryQuantity: newQuantity,
                 }
                 var oModel = this.getView().getModel();
 
-                oModel.create(sPath,data,{
-                    success: function(oData,oResponse){
+                oModel.create(sPath, data, {
+                    success: function (oData, oResponse) {
                         console.log(oData);
                         console.log(oResponse);
 
                         var message = oResponse.headers['sap-message'];
                         var message_obj = JSON.parse(message);
-                        if(message_obj.details.length != 0){
-                            var content = message_obj.details.map(function(element){
+                        if (message_obj.details.length != 0) {
+                            var content = message_obj.details.map(function (element) {
                                 return new sap.m.StandardListItem({
-                                    title : element.message,
+                                    title: element.message,
                                     //description : element.message,
                                 })
                             });
                         }
-      
-                        this._showMessageDialog(message_obj.message,message_obj.severity,content)
+
+                        this._showMessageDialog(message_obj.message, message_obj.severity, content)
 
                     }.bind(this),
-                    error: function(error){
+                    error: function (error) {
                         console.log(error);
                     }
                 });
 
             },
-            onShippingPointValueHelp: function(oEvent){
+            onShippingPointValueHelp: function (oEvent) {
                 var sInputValue = oEvent.getSource().getValue()
 
-                if(!this._oShippingPointValueHelpDialog){
-                    this._oShippingPointValueHelpDialog = sap.ui.xmlfragment(this.getView().getId(),"salesapp.view.shippingPointValueHelp", this);
+                if (!this._oShippingPointValueHelpDialog) {
+                    this._oShippingPointValueHelpDialog = sap.ui.xmlfragment(this.getView().getId(), "salesapp.view.shippingPointValueHelp", this);
                     this.getView().addDependent(this._oShippingPointValueHelpDialog);
                 }
-    
+
                 this._oShippingPointValueHelpDialog.getBinding("items").filter([new Filter("ShippingPoint", FilterOperator.Contains, sInputValue)]);
                 this._oShippingPointValueHelpDialog.open();
             },
-            onValueHelpSearch: function(oEvent){
+            onValueHelpSearch: function (oEvent) {
                 var sValue = oEvent.getParameter("value");
                 var oFilter = new Filter("ShippingPoint", FilterOperator.Contains, sValue);
                 oEvent.getSource().getBinding("items").filter([oFilter]);
 
             },
-            onValueHelpClose: function(oEvent){
+            onValueHelpClose: function (oEvent) {
                 var oSelectedItem = oEvent.getParameter("selectedItem");
                 oEvent.getSource().getBinding("items").filter([]);
-    
+
                 if (!oSelectedItem) {
                     return;
                 }
-    
+
                 this.byId("ShippingPointValue").setValue(oSelectedItem.getTitle());
 
             },
-            _closeDialog: function(){
+            _closeDialog: function () {
                 this._oDialog.close();
                 this._oDialog.destroy();
                 this._oDialog = undefined;
+            },
+            onJSON: function () {
+                var item = this.byId('table').getSelectedItems()[0];
+
+                if (!item) {
+                    MessageToast.show("Select a sales order item");
+                    return;
+                }
+                var SalesOrder = item.getBindingContext().getProperty('SalesOrder');
+                var SalesOrderItem = item.getBindingContext().getProperty('SalesOrderItem');
+
+                var oModel = this.getView().getModel();
+                oModel.callFunction("/getOrderJSON", {
+                    method: "POST",
+                    urlParameters: {
+                        SalesOrder: SalesOrder,
+                        SalesOrderItem: SalesOrderItem,
+                    },
+                    success: function (oData, response) {
+
+                        var json_string = response.data.getOrderJSON.JsonString;
+
+                        if (!this._oDialogJSON) {
+                            this._oDialogJSON = sap.ui.xmlfragment(this.getView().getId(), "salesapp.view.OrderJSON", this);
+                            this.getView().addDependent(this._oDialogJSON);
+                        }
+                        var payLoadData = {
+                            payload: []
+                        };
+                        var payloadModel = new JSONModel(payLoadData);
+                        var jsonPretty = JSON.stringify(JSON.parse(json_string), null, 2);
+                        payloadModel.setProperty('/payload', jsonPretty);
+                        this._oDialogJSON.setModel(payloadModel, 'payloadModel');
+                        this._oDialogJSON.open();
+
+                    }.bind(this),
+                    error: function (error) {
+                        MessageToast.show(error);
+                    }
+                }
+                )
+            },
+            closePayloadDsp: function () {
+                this._oDialogJSON.close();
             }
         });
     });
